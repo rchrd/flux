677be9690a58742b72555a2175f8a7ced8da5887
Richard Woudenberg <rchrd@users.noreply.github.com>
2020-06-08 09:26:55 +0200

    Update installer

--- flux-init.sh
+++ flux-init.sh
@@ -17,27 +17,26 @@ fi
 helm repo add fluxcd https://charts.fluxcd.io
 
 echo ">>> Installing Flux"
-kubectl create ns flux || true
+kubectl create ns flux-system || true
 helm upgrade -i flux fluxcd/flux \
 --wait \
 --values flux-values.yaml \
---namespace flux
+--namespace flux-system
 
 echo ">>> Installing Helm Operator"
-# kubectl apply -f https://raw.githubusercontent.com/fluxcd/helm-operator/1.1.0/deploy/crds.yaml
+kubectl apply -f https://raw.githubusercontent.com/fluxcd/helm-operator/1.1.0/deploy/crds.yaml
 helm upgrade -i helm-operator fluxcd/helm-operator \
 --wait \
 --set git.ssh.secretName=flux-git-deploy \
 --set helm.versions=v3 \
---set createCRD=true \
---namespace flux
+--namespace flux-system
 
 echo ">>> GitHub deploy key"
-kubectl -n flux logs deployment/flux | grep identity.pub | cut -d '"' -f2
+kubectl -n flux-system logs deployment/flux | grep identity.pub | cut -d '"' -f2
 
 # wait until flux is able to sync with repo
 echo ">>> Waiting on user to add above deploy key to Github with write access"
-until kubectl logs -n flux deployment/flux | grep event=refreshed
+until kubectl logs -n flux-system deployment/flux | grep event=refreshed
 do
   sleep 5
 done
